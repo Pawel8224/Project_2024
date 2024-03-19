93b78dbc8d315237e2695bbc74aceb41902b68a4
Pawel8224 <pawel.milewski01@gmail.com>
2024-03-19 14:17:06 +0100

    input default currency option and message

--- echange_rates/echange_main.py
+++ echange_rates/echange_main.py
@@ -1,39 +1,34 @@
-
 from tkinter import *
 import requests
-
+from tkinter import messagebox
 
 
 #############  DEF   #############
 
-
 def act_rate():
-
-    aft_value = float(ammount.get())
+    global aft_value
     aft_currency = option_var.get()
     body = requests.get('http://api.nbp.pl/api/exchangerates/tables/A/')
     response = body.json()
 
 
-
-    for rate in response[0]['rates']:
-        print(rate['code'],rate['currency'],rate['mid'])
-        if rate['code'] == aft_currency:
-            currency_new = str(rate['code'])
-            value_new = float(rate['mid'])
-            sum = '%.2f' % (aft_value / float(value_new))
-
-
-
-    Label(root, text='Waluta: ' + currency_new, font='calibri 20').place(x=100, y=250)
-    Label(root, text='Kurs: ' + str(value_new),font='calibri 20').place(x=100, y=285)
-    Label(root, text='Otrzymasz: ' + sum,font='calibri 20').place(x=100, y=320)
-
-
+    try:
+        aft_value = float(ammount.get())
+        for rate in response[0]['rates']:
+            if rate['code'] == aft_currency:
+                currency_new = str(rate['code'])
+                value_new = float(rate['mid'])
+                sum = '%.2f' % (aft_value / float(value_new))
+        Label(root, text='Waluta: ' + currency_new, font='calibri 20').place(x=90, y=230)
+        Label(root, text='Kurs: ' + str(value_new), font='calibri 20').place(x=90, y=270, )
+        Label(root, text='Otrzymasz: ' + sum, font='calibri 20').place(x=90, y=310)
+    except:
+        messagebox.showinfo(title='Informacja', message='Nie podałeś wartości KWOTA')
 
 
 
 ############# TKINTER  #############
+
 
 root = Tk()
 root.title('Przelicznik kursów walut')
@@ -43,29 +38,38 @@ root.resizable(width=False, height=False)
 canvas = Canvas(root, width=600, height=600)
 canvas.pack(fill='both')
 
-Label(root,text ="Przelicznik kursów walut PLN", font = 'calibri 20 bold').place(x=70,y=15)
+bg_photo = PhotoImage(file='/Users/pablom/PycharmProjects/Projekty2024/echange_rates/bbg.png')
+canvas.create_image(0, 0, image=bg_photo, anchor='nw')
+
+canvas.create_text(200, 30, text='Wymień walute PL', font='calibri 20 bold', fill='black')
 
 # Ilosc
-Label(root, text='Kwota:',font = 'calibri 19 bold').place(x=30,y=80)
+canvas.create_text(50, 95, text='Kwota:', font='calibri 19 bold', fill='black')
+
 amount_s = IntVar()
-ammount = Entry(root, textvariable=amount_s, width=13, font='calibri 16',fg = 'black', bd=0,bg='white')
+ammount = Entry(root, textvariable=amount_s, width=13, font='calibri 16', fg='black', bd=0, bg='white')
 ammount.place(x=110, y=83)
 
-
 # Waluta do przeliczenia
+canvas.create_text(50, 145, text='Waluta:', font='calibri 19 bold', fill='black')
 
-Label(root, text='Waluta:',font = 'calibri 19 bold').place(x=30,y=130)
+currency_option = ['THB', 'GBP', 'USD', 'EUR', 'AUD', 'HUF', 'CHF', 'CZK', 'NOK','TRY']
 
-currency_option = ('THB', 'GBP', 'USD','EUR','AUD','HUF','CHF','CZK','NOK','TRY')
-option_var= StringVar()
-currency= OptionMenu(root, option_var,*currency_option)
-currency.place(x=120,y=135)
+option_var = StringVar()
+option_var.set(currency_option[3])
+currency = OptionMenu(root, option_var, *currency_option)
+currency.place(x=110, y=135)
 
 # Nowa sekcja - wynik przewalutowania
-Label(root, text='Przewalutowanie', font = 'calibri 20 bold').place(x=120,y=190)
-
+canvas.create_text(190, 190, text='Przewalutowanie', font='calibri 20 bold', fill='black')
 
 # Button -  pokaz wynik
-Button(root,text= 'Sprawdź kurs/kwote', font ='calibri 16', fg='black', bg = 'white', command = act_rate).place(x=100,y=380)
+Button(root, text='Sprawdź kurs/kwote', font='calibri 16', fg='black', bg='white', command=act_rate).place(x=100, y=380)
+
+# Wyświetlanie wyniku
+# lbl_curr = Label(root, text = '', font='calibri 20').place(x=140, y=250)
+# lbl_value = Label(root, text = '', font='calibri 20').place(x=140, y=280)
+# lbl_sum = Label(root, text = '', font='calibri 20').place(x=140, y=300)
+
 
 root.mainloop()
