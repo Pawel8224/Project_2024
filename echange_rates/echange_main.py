1f3c6ee2914726dc2236400ffe3280c118041150
Pawel8224 <pawel.milewski01@gmail.com>
2024-03-19 20:20:21 +0100

    new window with mid

--- echange_rates/echange_main.py
+++ echange_rates/echange_main.py
@@ -1,4 +1,5 @@
 from tkinter import *
+import tkinter as tk
 import requests
 from tkinter import messagebox
 
@@ -10,7 +11,6 @@ def act_rate():
     aft_currency = option_var.get()
     body = requests.get('http://api.nbp.pl/api/exchangerates/tables/A/')
     response = body.json()
-
 
     try:
         aft_value = float(ammount.get())
@@ -25,6 +25,33 @@ def act_rate():
     except:
         messagebox.showinfo(title='Informacja', message='Nie podałeś wartości KWOTA')
 
+
+def new_window_history():
+    new_windows = tk.Toplevel()
+    new_windows.title('Przelicznik kursów walut')
+    new_windows.geometry('400x600')
+    new_windows.resizable(width=False, height=False)
+
+    Label(new_windows, text='Kursy z dnia poprzedniego', font='calibri 19').place(x=80, y=10)
+
+    body1 = requests.get('http://api.nbp.pl/api/exchangerates/tables/A/2024-03-18/2024-03-18/')
+    response1 = body1.json()
+
+    frame_show_cur = Frame(new_windows, width=60, height=30, bd=0, bg='white')
+    frame_show_cur.place(x=20, y=60)
+
+    show_cur = Listbox(frame_show_cur, font=('calibri', 14), width=40, height=28, bg='#e4eaf5', fg='black')
+    show_cur.pack(side=LEFT, fill=BOTH)
+
+    for rate1 in response1[0]['rates']:
+        cur = str(rate1['currency'])
+        cod = str(rate1['code'])
+        val = float(rate1['mid'])
+
+        text_sum = "%s | %s | Kurs: %s" % (cod, cur, float(val))
+        show_cur.insert(END, text_sum)
+
+    # Wyświetl kursy
 
 
 ############# TKINTER  #############
@@ -53,7 +80,7 @@ ammount.place(x=110, y=83)
 # Waluta do przeliczenia
 canvas.create_text(50, 145, text='Waluta:', font='calibri 19 bold', fill='black')
 
-currency_option = ['THB', 'GBP', 'USD', 'EUR', 'AUD', 'HUF', 'CHF', 'CZK', 'NOK','TRY']
+currency_option = ['THB', 'GBP', 'USD', 'EUR', 'AUD', 'HUF', 'CHF', 'CZK', 'NOK', 'TRY']
 
 option_var = StringVar()
 option_var.set(currency_option[3])
@@ -66,10 +93,14 @@ canvas.create_text(190, 190, text='Przewalutowanie', font='calibri 20 bold', fil
 # Button -  pokaz wynik
 Button(root, text='Sprawdź kurs/kwote', font='calibri 16', fg='black', bg='white', command=act_rate).place(x=100, y=380)
 
-# Wyświetlanie wyniku
-# lbl_curr = Label(root, text = '', font='calibri 20').place(x=140, y=250)
-# lbl_value = Label(root, text = '', font='calibri 20').place(x=140, y=280)
-# lbl_sum = Label(root, text = '', font='calibri 20').place(x=140, y=300)
+# Button - pokaż dane historyczne
+Button(root, text='Pokaż historie kursów', font='calibri 16', fg='black', bg='white', command=new_window_history).place(
+    x=94, y=430)
 
+# (new_window) Frame - wyświetl wynik
+
+
+# Dodatkowe info
+canvas.create_text(190, 480, text='Dane z API: NBP https://api.nbp.pl/,', font='calibri 13', fill='white')
 
 root.mainloop()
